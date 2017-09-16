8a7ab6411688217418cc0c846b4a32775f791ce8
mmmmeng3 <rain950617@gmail.com>
2017-09-15 22:12:43 -0400

    assets added

---
+++ rolling balls/first tut game/Assets/Script/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+	
+	private Rigidbody rb;
+
+	public float speed;
+
+	void Start(){
+		rb =GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+		float moveH = Input.GetAxis("Horizontal");
+		float moveV = Input.GetAxis("Vertical");
+
+		Vector3 movement =new Vector3(moveH,0.0f,moveV);
+
+		rb.AddForce(movement* speed);
+	}
+}
