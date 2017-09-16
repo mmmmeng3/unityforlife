3605c1cf640b0a3a5de0c2a4c8fca9df42ece78a
mmmmeng3 <rain950617@gmail.com>
2017-09-15 23:09:07 -0400

    done first game tut

--- rolling balls/first tut game/Assets/Script/playerController.cs
+++ rolling balls/first tut game/Assets/Script/playerController.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
 	
+	
+
+	public Text countText;
+	
+	public float speed;
+
+	public	Text win; 
+	
 	private Rigidbody rb;
 
-	public float speed;
+	private int count;
+
 
 	void Start(){
 		rb =GetComponent<Rigidbody>();
+		count =0;
+		countText.text ="Point: "+ count.ToString();
+		win.text = "";
 	}
 
 	void FixedUpdate(){
@@ -19,5 +32,16 @@ public class playerController : MonoBehaviour {
 		Vector3 movement =new Vector3(moveH,0.0f,moveV);
 
 		rb.AddForce(movement* speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			countText.text ="Point: "+ count.ToString();
+			if(count >=4){
+				win.text ="You win!";
+			}
+		}
 	}
 }
